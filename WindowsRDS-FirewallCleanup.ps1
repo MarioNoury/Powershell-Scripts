36672b2cb208fc9509f9c8f7e798ddb99120d250
Mario Noury <48540442+MarioNoury@users.noreply.github.com>
2021-07-19 11:39:55 -0400

    Update WindowsRDS-FirewallCleanup.ps1

--- WindowsRDS-FirewallCleanup.ps1
+++ WindowsRDS-FirewallCleanup.ps1
@@ -75,7 +75,7 @@ switch ($targetos)
  }
 
 # Registry patch
-New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy' -Name  'DeleteUserAppContainersOnLogoff' -Value '1' -PropertyType 'DWORD' –Force
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy' -Name  'DeleteUserAppContainersOnLogoff' -Value '1' -PropertyType 'DWORD' –Force | out-null
 Write-Host "Registry patched !" -ForegroundColor Green
 
 #Start cleanup script
